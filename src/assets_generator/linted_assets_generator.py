04dd8e8f665cddca68bc6532cb3ce8c8cff8d541
Steven Stewart-Gallus <sstewartgallus00@mylangara.bc.ca>
2014-02-26 12:48:00 -0800

    Fix copyright dates

--- src/assets_generator/linted_assets_generator.py
+++ src/assets_generator/linted_assets_generator.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2013 Steven Stewart-Gallus
+# Copyright 2013, 2014 Steven Stewart-Gallus
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you
 # may not use this file except in compliance with the License.  You may
