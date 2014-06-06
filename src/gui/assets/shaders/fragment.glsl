e04c934f2ec181be08a834cefb6f2593197da8ab
Steven Stewart-Gallus <sstewartgallus00@mylangara.bc.ca>
2014-06-06 16:04:19 -0700

    Improve the shading

--- src/gui/assets/shaders/fragment.glsl
+++ src/gui/assets/shaders/fragment.glsl
@@ -21,14 +21,20 @@ precision highp float;
 const vec3 light_location = vec3(-0.5, 1.0, 0.0);
 const vec3 colour = vec3(1.0, 0.9, 1.0);
 
+const vec3 dark_stone = vec3(0.1, 0.3, 0.5);
+const vec3 foggy_air = vec3(1.0, 0.4, 0.2);
+
 void main()
 {
     float impact = max(0.0, dot(normalize(linted_varying_normal),
                                 normalize(light_location - linted_varying_vertex)));
-    float intensity = 1.0 + impact + impact * impact;
+    float intensity = dot(dark_stone,
+                          vec3(1.0, impact, impact * impact));
 
     float delta = distance(light_location, linted_varying_vertex);
-    float decay = 1.0 + delta + delta * delta;
+
+    float decay = dot(foggy_air,
+                      vec3(1.0, delta, delta * delta));
 
     gl_FragColor = vec4((intensity / decay) * colour, 1.0);
 }
