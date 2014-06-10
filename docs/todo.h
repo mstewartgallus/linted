5d14fe590da6d5ae8652f749961d9636d0a5c66b
Steven Stewart-Gallus <sstewartgallus00@mylangara.bc.ca>
2014-06-09 17:56:15 -0700

    Cleanup todo list

--- docs/todo.h
+++ docs/todo.h
@@ -11,33 +11,22 @@
  *
  * Linted -- TODO
  *
- * @todo Port to --host=i686-w64-mingw32 and CC=winegcc CFLAGS='-mno-cygwin -maccumulate-outgoing-args -std=gnu99'
+ * @todo Port to --host=x86_64-w64-mingw32
  * - As part of this process environment sanitization code will need
  *   to be reworked. Windows maintains two copies of environment
  *   variables: one copy in process space and the other copy in kernel
  *   space.
  *
- * @todo Move away from C legacy practises
- * - move away from null terminated strings
+ * @todo Use length annotated strings instead of C strings.
  *
- * @todo Solve banding problems that are occuring in the shader
+ * @todo Stop graphical banding problems
  *
- * @todo Make hang check timers in init
- * - These timers will periodically make sure tasks are reaching their
- *   deadlines by sending a message to every message queue and seeing
- *   if the owner of the queue responds within appropriate
- *   deadlines. It will then kill and restart the process if it is not
- *   responding.
+ * @todo Check child processes for hangups
+ * - Periodically poll message channels to see if they are writeable
+ *   and not full because the reader has hungup. If the reader has
+ *   hungup then kill and restart the reader.
  *
- * @todo Move shell scripts to Python.
+ * @todo Rewrite utility shell scripts in Python.
  *
- * @todo Monitor vulnerabilities in dependencies and supported platforms
- *
- * @todo Make a minor and a major debug mode
- *
- * @todo Think about whether to use -Wa,--noexecstack and -Wl,-z,noexecstack
- * - I think I read somewhere that these force a nonexecutable stack
- *   but that if things are done correctly that the shouldn't be
- *   happening anyways. I believe what I really want is a
- *   -Wl,--warn-execstack option.
+ * @todo Create a feed of possible vulnerabilities in all dependencies
  */
