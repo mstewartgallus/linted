bc6927cd3a3b388041981156543da9db4faa2eb8
Steven Stewart-Gallus <sstewartgallus00@mylangara.bc.ca>
2014-08-30 13:30:22 -0700

    Remove outdated documentation

--- docs/todo.h
+++ docs/todo.h
@@ -47,21 +47,12 @@
  *   security) of OSS/FS programs.
  *
  * @todo Port to --host=x86_64-w64-mingw32
- * - As part of this process environment sanitization code will need
- *   to be reworked. Windows maintains two copies of environment
- *   variables: one copy in process space and the other copy in kernel
- *   space.
  *
  * @todo Use length annotated strings instead of C strings.
  *
  * @todo Stop graphical banding problems
  *
  * @todo Check child processes for hangups
- * - Periodically poll message channels to see if they are writeable
- *   and not full because the reader has hungup. If the reader has
- *   hungup then kill and restart the reader.
- *
- * @todo Rewrite utility shell scripts in Python.
  *
  * @todo Use the monitor to report crashes such as from seccomp rule
  *       violations.
