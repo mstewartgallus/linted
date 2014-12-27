8e65761aa782b283145a9e1819088ad4b532ccb6
Steven Stewart-Gallus <sstewartgallus00@mylangara.bc.ca>
2014-12-27 14:56:35 -0800

    Fix

--- include/linted/asynch.h
+++ include/linted/asynch.h
@@ -59,8 +59,8 @@ void linted_asynch_pool_complete(struct linted_asynch_pool *pool,
 
 void linted_asynch_pool_wait_on_poll(struct linted_asynch_pool *pool,
                                      struct linted_asynch_waiter *waiter,
-                                     struct linted_asynch_task *task, void *ko,
-                                     short flags);
+                                     struct linted_asynch_task *task,
+                                     unsigned ko, short flags);
 
 linted_error linted_asynch_pool_wait(struct linted_asynch_pool *pool,
                                      struct linted_asynch_task **completionp);
