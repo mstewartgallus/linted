c3ee4b7d3c27b39ccbba92ddb1a411caff37a218
Steven Stewart-Gallus <sstewartgallus00@mylangara.bc.ca>
2015-01-03 13:03:19 -0800

    Cleanup process management docs

--- docs/process-management.h
+++ docs/process-management.h
@@ -10,7 +10,7 @@
  *
  * Linted -- Process Management
  *
- * Process heirarchy
+ * Process hierarchy
  *
  * - `init`
  *   - `simulator`
@@ -19,14 +19,12 @@
  *      - `drawer`
  *   - `gui`
  *     - `gui`
- *   - `logger`
- *     - `logger`
  *   - `window`
  *     - `window`
  *   - `monitor`
  *
  * `init` is the top level service that contains everything using
- * `PR_SET_CHILD_SUBREAPER`. All it does is monitor and restart the
+ * `PR_SET_CHILD_SUBREAPER`.  All it does is monitor and restart the
  * `monitor` process and delegate signals to it.
  *
  * Underneath `init` the `monitor` process monitors and restarts
@@ -34,33 +32,31 @@
  *
  * Service processes underneath `init` using `PR_SET_CHILD_SUBREAPER`
  * or `CLONE_NEWPID` contain underneath them similarly named processes
- * that do the actual work of the service . Service processes don't
- * actually monitor or restart their children. That job is delegated
- * to `monitor` who ptraces and monitor these service processes.
+ * that do the actual work of the service.  Service processes don't
+ * actually monitor or restart their children.  That job is delegated
+ * to `monitor` who ptraces and monitor these service processes (but
+ * not the children of the service processes.)
  *
- * @bug Currently if `init` dies all the child processes will die too
- * from `SIGHUP` because `init` will be the controlling process for
- * the controlling terminal (if there is a terminal.) This is not good
- * enough for proper containerization as it doesn't work when there is
- * not a controlling terminal or if children change their controlling
- * terminal.
+ * @bug Currently not all sandboxed processes are killed on `init`
+ * death.  Currently `monitor` will receive a `SIGHUP` because `init`
+ * will be the controlling process for the controlling terminal (if
+ * there is a terminal) or from a `SIGKILL` from set parent death
+ * signal.  Possible solutions:
+ * - `monitor` could ptrace `init` and kill its children when `init`
+ *   is about to die.
+ * - Sandboxes could use `PR_SET_PDEATHSIG` once they have been
+ *   reparented to `init`.  However, I am not sure how they would wait
+ *   until they have been reparented to `init` before using
+ *   `PR_SET_PDEATHSIG`.
  *
- * Currently if `CLONE_NEWPID` sandboxes die then their children will
- * automatically be killed by the kernel.
- *
- * @bug Go back to the main loop to kill `PR_SET_CHILD_SUBREAPER`
- * sandbox children when `monitor` is exiting and killing them.
+ * `monitor` manually kill any children of dying
+ * `PR_SET_CHILD_SUBREAPER` type sandboxes and children of dying
+ * `CLONE_NEWPID` type sandboxes are killed by the kernel
+ * automatically.
  *
  * @section signals Signal handling
  *
- * Currently `init` blocks all nonurgent exit signals (`SIGHUP`,
- * `SIGINT1, `SIGQUIT`, and `SIGTERM`) and lets the `monitor` handle
+ * Currently `init` forwards all nonurgent exit signals (`SIGHUP`,
+ * `SIGINT1`, `SIGQUIT`, and `SIGTERM`) to the `monitor` to handle
  * them.
- *
- * Currently as sandboxes are ptraced by the `monitor` when they get
- * terminal notifications such as `SIGINT` they are not acted upon as
- * `monitor` traps such notifications and doesn't process them while
- * it is shutting down. This may be a good way of preventing sandboxes
- * from getting terminal notifications or it may be a bad way of
- * handling this.
  */
