47727016b2d2cdabadde8205cb13cedfd9355b37
Steven Stewart-Gallus <sstewartgallus00@mylangara.bc.ca>
2017-02-08 12:37:23 -0800

    Add performance optimization

--- src/ada-core/src/linted-wait_lists.adb
+++ src/ada-core/src/linted-wait_lists.adb
@@ -23,6 +23,7 @@ package body Linted.Wait_Lists with
    end record;
 
    procedure Insert (W : in out Wait_List; N : Node_Access);
+   procedure Insert_Lots (W : in out Wait_List; Root : Node_Access);
 
    procedure Insert (W : in out Wait_List; N : Node_Access) is
       Head : Node_Access;
@@ -36,20 +37,47 @@ package body Linted.Wait_Lists with
       end loop;
    end Insert;
 
+   procedure Insert_Lots (W : in out Wait_List; Root : Node_Access) is
+      Success : Boolean;
+      Current_Root : Node_Access;
+   begin
+      Current_Root := Root;
+      while Current_Root /= null loop
+         --  Perf opt in the case that there are no waiters
+         Node_Access_Atomics.Compare_And_Swap
+           (W.Root,
+            null,
+            Current_Root,
+            Success);
+         if Success then
+            exit;
+         end if;
+
+         declare
+            Next : Node_Access;
+         begin
+            Next := Current_Root.Next;
+            Current_Root.Next := null;
+            Insert (W, Current_Root);
+            Current_Root := Next;
+         end;
+      end loop;
+   end Insert_Lots;
+
    procedure Wait (W : in out Wait_List) is
       Is_Triggered : Default_False;
    begin
       loop
-	 Boolean_Atomics.Get (W.Triggered, Is_Triggered);
-	 if Is_Triggered then
-	    exit;
-	 end if;
-	 declare
-	    N : aliased Node;
-	 begin
-	    Insert (W, N'Unchecked_Access);
-	    STC.Suspend_Until_True (N.Trigger);
-	 end;
+         Boolean_Atomics.Get (W.Triggered, Is_Triggered);
+         if Is_Triggered then
+            exit;
+         end if;
+         declare
+            N : aliased Node;
+         begin
+            Insert (W, N'Unchecked_Access);
+            STC.Suspend_Until_True (N.Trigger);
+         end;
       end loop;
       Boolean_Atomics.Set (W.Triggered, False);
    end Wait;
@@ -61,14 +89,14 @@ package body Linted.Wait_Lists with
       Node_Access_Atomics.Swap (W.Root, Root, null);
 
       while Root /= null loop
-	 declare
-	    Next : Node_Access;
-	 begin
-	    Next := Root.Next;
-	    Root.Next := null;
-	    STC.Set_True (Root.Trigger);
-	    Root := Next;
-	 end;
+         declare
+            Next : Node_Access;
+         begin
+            Next := Root.Next;
+            Root.Next := null;
+            STC.Set_True (Root.Trigger);
+            Root := Next;
+         end;
       end loop;
    end Broadcast;
 
@@ -79,27 +107,18 @@ package body Linted.Wait_Lists with
       Node_Access_Atomics.Swap (W.Root, Root, null);
 
       if null = Root then
-	 return;
+         return;
       end if;
 
       declare
-	 Next : Node_Access;
+         Next : Node_Access;
       begin
-	 Next := Root.Next;
-	 Root.Next := null;
-	 STC.Set_True (Root.Trigger);
-	 Root := Next;
+         Next := Root.Next;
+         Root.Next := null;
+         STC.Set_True (Root.Trigger);
+         Root := Next;
       end;
 
-      while Root /= null loop
-	 declare
-	    Next : Node_Access;
-	 begin
-	    Next := Root.Next;
-	    Root.Next := null;
-	    Insert (W, Root);
-	    Root := Next;
-	 end;
-      end loop;
+      Insert_Lots (W, Root);
    end Signal;
 end Linted.Wait_Lists;
