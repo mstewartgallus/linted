9761991eeae42e58435037cd6e47e1d6a59f87cc
Steven Stewart-Gallus <sstewartgallus00@mylangara.bc.ca>
2017-02-08 19:49:42 -0800

    Solve the ABA problem

--- src/ada-core/src/linted-lock_free_stack.adb
+++ src/ada-core/src/linted-lock_free_stack.adb
@@ -11,6 +11,8 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
 -- implied.  See the License for the specific language governing
 -- permissions and limitations under the License.
+with Interfaces;
+
 with Linted.Atomics;
 
 package body Linted.Lock_Free_Stack with
@@ -18,19 +20,44 @@ package body Linted.Lock_Free_Stack with
 is
    type My_Ix is new Ix with
         Default_Value => 0;
+   type ABA is mod 2**16 with
+        Default_Value => 0;
+   type ABA_Ix is new Interfaces.Unsigned_32 with
+        Default_Value => 0;
 
-   package My_Atomics is new Atomics (My_Ix);
+   function Make_ABA_Ix (My_Index : My_Ix; My_Tag : ABA) return ABA_Ix;
+   function Index (X : ABA_Ix) return My_Ix;
+   function Tag (X : ABA_Ix) return ABA;
+
+   package Aba_Atomics is new Atomics (ABA_Ix);
+   package Access_Atomics is new Atomics (My_Ix);
 
    type Element_Array is array (My_Ix range 1 .. My_Ix (Ix'Last)) of Element_T;
 
    Buf_Contents : Element_Array;
 
    type Node_Array is
-     array (My_Ix range 1 .. My_Ix (Ix'Last)) of My_Atomics.Atomic;
+     array (My_Ix range 1 .. My_Ix (Ix'Last)) of Access_Atomics.Atomic;
 
    Buf_Nodes : Node_Array;
-   Buf_Head : My_Atomics.Atomic;
-   Buf_Free : My_Atomics.Atomic;
+   Buf_Head : Aba_Atomics.Atomic;
+   Buf_Free : Aba_Atomics.Atomic;
+
+   function Make_ABA_Ix (My_Index : My_Ix; My_Tag : ABA) return ABA_Ix is
+   begin
+      return ABA_Ix (My_Index) or
+        ABA_Ix (Interfaces.Shift_Left (Interfaces.Unsigned_32 (My_Tag), 16));
+   end Make_ABA_Ix;
+
+   function Index (X : ABA_Ix) return My_Ix is
+   begin
+      return My_Ix (X and 16#FFFF#);
+   end Index;
+
+   function Tag (X : ABA_Ix) return ABA is
+   begin
+      return ABA (Interfaces.Shift_Right (Interfaces.Unsigned_32 (X), 16));
+   end Tag;
 
    procedure Allocate (Free : out My_Ix) with
       Global => (In_Out => (Buf_Nodes, Buf_Free)),
@@ -46,12 +73,12 @@ is
       (Buf_Nodes => (Buf_Nodes, Buf_Free, Head),
        Buf_Free => (Buf_Free, Head));
 
-   procedure Push_Node (N : in out My_Atomics.Atomic; Free : My_Ix) with
+   procedure Push_Node (N : in out Aba_Atomics.Atomic; Free : My_Ix) with
       Pre => Free /= 0,
       Global => (In_Out => Buf_Nodes),
       Depends => (N => (Free, N), Buf_Nodes => (Free, Buf_Nodes, N));
 
-   procedure Pop_Node (N : in out My_Atomics.Atomic; Head : out My_Ix) with
+   procedure Pop_Node (N : in out Aba_Atomics.Atomic; Head : out My_Ix) with
       Global => (In_Out => Buf_Nodes),
       Depends =>
       (Head => (Buf_Nodes, N),
@@ -85,14 +112,18 @@ is
       end if;
    end Try_Push;
 
-   procedure Push_Node (N : in out My_Atomics.Atomic; Free : My_Ix) is
-      Head : My_Ix;
+   procedure Push_Node (N : in out Aba_Atomics.Atomic; Free : My_Ix) is
+      Head : ABA_Ix;
       Swapped : Boolean;
    begin
       loop
-         My_Atomics.Get (N, Head);
-         My_Atomics.Set (Buf_Nodes (Free), Head);
-         My_Atomics.Compare_And_Swap (N, Head, Free, Swapped);
+         Aba_Atomics.Get (N, Head);
+         Access_Atomics.Set (Buf_Nodes (Free), Index (Head));
+         Aba_Atomics.Compare_And_Swap
+           (N,
+            Head,
+            Make_ABA_Ix (Free, Tag (Head) + 1),
+            Swapped);
          exit when Swapped;
       end loop;
    end Push_Node;
@@ -125,19 +156,25 @@ is
       end if;
    end Try_Pop;
 
-   procedure Pop_Node (N : in out My_Atomics.Atomic; Head : out My_Ix) is
+   procedure Pop_Node (N : in out Aba_Atomics.Atomic; Head : out My_Ix) is
       New_Head : My_Ix;
       Swapped : Boolean;
+      ABA_Head : ABA_Ix;
    begin
       loop
-         My_Atomics.Get (N, Head);
-         if Head = 0 then
+         Aba_Atomics.Get (N, ABA_Head);
+         if Index (ABA_Head) = 0 then
             exit;
          end if;
-	 My_Atomics.Get (Buf_Nodes (Head), New_Head);
-	 My_Atomics.Compare_And_Swap (N, Head, New_Head, Swapped);
-	 exit when Swapped;
+         Access_Atomics.Get (Buf_Nodes (Index (ABA_Head)), New_Head);
+         Aba_Atomics.Compare_And_Swap
+           (N,
+            ABA_Head,
+            Make_ABA_Ix (New_Head, Tag (ABA_Head) + 1),
+            Swapped);
+         exit when Swapped;
       end loop;
+      Head := Index (ABA_Head);
    end Pop_Node;
 
 begin
@@ -148,6 +185,6 @@ begin
          Buf_Nodes (II).Set (Old_Node);
          Old_Node := II;
       end loop;
-      Buf_Free.Set (Old_Node);
+      Buf_Free.Set (Make_ABA_Ix (Old_Node, 0));
    end;
 end Linted.Lock_Free_Stack;
